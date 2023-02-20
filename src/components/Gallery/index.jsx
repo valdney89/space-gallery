a9bc1dd731d6a9e82f33f0f8d00c72e46d22a61c
Valdney <vnogueira@translucentcomputing.com>
2023-02-20 16:17:34 -0300

    feat: add funcionalities

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Tags from '../Tags'
 import styles from './Gallery.module.scss'
 import photos from './photos.json'
@@ -6,36 +6,50 @@ import open from '../../assets/images/open.png'
 import favorite from '../../assets/images/favorito.png'
 
 export default function Gallery() {
-  return (
-    <section className={styles.gallery}>
-        <h2>Navegue pela galeria</h2>
-        <Tags />
-        <ul className={styles.gallery__cards}>
-            {
-                photos.map((photo) => {
-                    return (
-                        <li
-                            key={photo.id}
-                            className={styles.gallery__card}
-                        >
-                            <img 
-                                src={photo.image} 
-                                alt={photo.title}
-                                className={styles.gallery__image}
-                            />
-                            <p className={styles.gallery__description}>{photo.title}</p>
-                            <div>
-                                <p>{photo.credits}</p>
-                                <span>
-                                    <img src={open} alt="" />
-                                    <img src={favorite} alt="" />
-                                </span>
-                            </div>
-                        </li>
-                    )
-                })
-            }
-        </ul>
-    </section>
-  )
+
+    const [itens, setItens] = useState(photos)
+    const tags = [...new Set(photos.map(value => value.tag))]
+
+    const filterPhotos = (tag) => {
+        const newPhotos = photos.filter(photo => photo.tag === tag)
+
+        setItens(newPhotos)
+    }
+
+    const resetFilter = () => {
+        setItens(photos)
+    }
+
+    return (
+        <section className={styles.gallery}>
+            <h2>Navegue pela galeria</h2>
+            <Tags tags={tags} filterPhotos={filterPhotos} onResetFilter={resetFilter} />
+            <ul className={styles.gallery__cards}>
+                {
+                    itens.map((photo) => {
+                        return (
+                            <li
+                                key={photo.id}
+                                className={styles.gallery__card}
+                            >
+                                <img 
+                                    src={photo.image} 
+                                    alt={photo.title}
+                                    className={styles.gallery__image}
+                                />
+                                <p className={styles.gallery__description}>{photo.title}</p>
+                                <div>
+                                    <p>{photo.credits}</p>
+                                    <span>
+                                        <img src={open} alt="" />
+                                        <img src={favorite} alt="" />
+                                    </span>
+                                </div>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        </section>
+    )
 }
--- src/components/Tags/index.jsx
+++ src/components/Tags/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import styles from './Tags.module.scss'
 
-export default function Tags() {
+export default function Tags({tags, filterPhotos, onResetFilter}) {
   return (
     <div className={styles.tags}>
         <p>Filtre por tags:</p>
         <ul className={styles.tags__list}>
-            <li>Estrelas</li>
-            <li>Galáxias</li>
-            <li>Lua</li>
-            <li>Planetas</li>
+            <li onClick={onResetFilter}>Todas</li>
+            {
+                tags.map(tag => <li key={tag} onClick={() => filterPhotos(tag)}>{tag}</li>)
+            }
         </ul>
     </div>
   )
